de81149cb66c6926feeca14d021a795414bfcdb7
Oliver Bryan <04oliverbryan@gmail.com>
2025-10-06 01:36:12 +0100

    use database in main

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"good_morning_backend/internal/database"
+	"good_morning_backend/internal/models"
 	"log"
 )
 
 func main() {
+	database.InitDB()
+	database.DB.AutoMigrate(&models.User{}, &models.Notice{})
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
