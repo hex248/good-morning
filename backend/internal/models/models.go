8c93b9c6b4d9b724d216bf6548e9846bcf6ac107
Oliver Bryan <04oliverbryan@gmail.com>
2025-10-06 01:34:08 +0100

    Notice model

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -18,6 +18,21 @@ type User struct {
 	UpdatedAt            time.Time `json:"updatedAt"`
 }
 
+type Notice struct {
+	ID              string     `gorm:"primaryKey" json:"id"`
+	SenderID        string     `json:"senderId"`
+	RecipientID     string     `json:"recipientId"`
+	Message         *string    `json:"message"`
+	PhotoURL        *string    `json:"photoUrl"`
+	SongURL         *string    `json:"songUrl"`
+	SongExplanation *string    `json:"songExplanation"`
+	Color           string     `json:"color"`
+	Reactions       []string   `gorm:"type:text[]" json:"reactions"`
+	SentAt          time.Time  `json:"sentAt"`
+	EditedAt        *time.Time `json:"editedAt"`
+	ResetAt         time.Time  `json:"resetAt"`
+}
+
 func (u *User) BeforeCreate(tx *gorm.DB) error {
 	u.CreatedAt = time.Now()
 	u.UpdatedAt = time.Now()
