560831d8598f68f0fde4e7af9f08820d4ab54b44
Oliver Bryan <04oliverbryan@gmail.com>
2025-10-07 02:21:04 +0100

    removed next-pwa

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
     images: {
@@ -12,8 +11,4 @@ const nextConfig: NextConfig = {
     },
 };
 
-export default withPWA({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
