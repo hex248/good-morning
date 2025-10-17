965cbb8e8a9438a949284d05e569e078f780c293
Oliver Bryan <04oliverbryan@gmail.com>
2025-10-18 00:18:08 +0100

    Revert "service worker"
    This reverts commit 90b8af3cffbe6a055ee74f1671c3cf55aafd43f5.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ServiceWorkerRegistration from "../components/ServiceWorkerRegistration";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -41,7 +40,6 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <ServiceWorkerRegistration />
                 {children}
             </body>
         </html>
